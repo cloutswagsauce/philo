c99243588c860e027ca051bbb88cc48507a22e41
luccamourao <lfaria@epfl.ch>
2024-12-19 17:29:04 +0100

    setting header

---
+++ philo.h
@@ -0,0 +1,18 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/time.h>
+ #include <pthread.h>
+
+
+
+
+
+
+#endif
+
+
